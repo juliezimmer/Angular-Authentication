02af455c0ed86dfb13e051b41d1ebde401bf366b
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2018-07-22 16:09:39 -0500

    added login service; returning users can now login

--- ngApp/src/app/auth.service.ts
+++ ngApp/src/app/auth.service.ts
@@ -7,10 +7,16 @@ import { HttpClient } from '@angular/common/http';
 export class AuthService {
    // stores backend api url
    private _registerUrl = "http://localhost:5000/api/register";
+   private _loginUrl = "http://localhost:5000/api/login";
 
    constructor(private http: HttpClient) { }
 
    registerUser(user){
       return this.http.post<any>(this._registerUrl, user)
+   }
+
+   loginUser(user){
+      // http 'get' request
+      return this.http.post<any>(this._loginUrl,user);  
    }
 }
