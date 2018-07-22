ccb1289408b10bac2f7ecdc7545d9fe5dd6fdbff
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2018-07-22 15:45:55 -0500

    added login UI

--- ngApp/src/app/login/login.component.ts
+++ ngApp/src/app/login/login.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+   selector: 'app-login',
+   templateUrl: './login.component.html',
+   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+   loginUserData = {}
 
-  constructor() { }
+   constructor() { }
 
-  ngOnInit() {
-  }
+   ngOnInit() {
+   }
 
+   loginUser(){
+      console.log(this.loginUserData);
+   }
 }
